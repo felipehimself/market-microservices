cebcd7cae12b0cbe5fe645a51e601fbbac5676bb
Felype Couto <felype_rocha@ecad.org.br>
2024-09-05 19:12:51 -0300

    feat: added logger Service to CrossCutting

--- Market.Inventory/src/Market.Inventory.Application/Services/ProductService.cs
+++ Market.Inventory/src/Market.Inventory.Application/Services/ProductService.cs
@@ -3,17 +3,21 @@ using Market.Inventory.Application.Dtos;
 using Market.Inventory.Domain.Entities;
 using Market.Inventory.Domain.Interfaces;
 using Market.Inventory.Application.Interfaces;
+using Market.Inventory.CrossCutting.Logger;
 
 namespace Market.Inventory.Application.Services
 {
-    public class ProductService(IProductRepository repository, IMapper mapper) : IProductService
+    public class ProductService(IProductRepository repository, IMapper mapper, ILoggerService<ProductService> logger) : IProductService
     {
         private readonly IProductRepository _repository = repository;
         private readonly IMapper _mapper = mapper;
+        private readonly ILoggerService<ProductService> _logger = logger;
 
 
         public async Task<ProductReadDto> CreateProductAsync(ProductCreateDto productDto)
         {
+
+        _logger.LogInformation("Creating Product");
 
         var entity = _mapper.Map<Product>(productDto);
            
@@ -26,13 +30,19 @@ namespace Market.Inventory.Application.Services
 
         public async Task<ProductReadDto> GetProductAsync(Guid id)
         {
+            _logger.LogInformation("Getting Product");
+
             var entity = await _repository.GetProductAsync(id);
+
+            if (entity == null) _logger.LogInformation("Not found, product is null");
 
             return _mapper.Map<ProductReadDto>(entity);
         }
 
         public async Task<IEnumerable<ProductReadDto>> GetProductsAsync()
-        {
+        {   
+            _logger.LogInformation("Getting Products");
+
             var products = await _repository.GetProductsAsync();
 
             return _mapper.Map<IEnumerable<ProductReadDto>>(products);
--- Market.Inventory/src/Market.Inventory.IoC/Configuration/ConfigServices.cs
+++ Market.Inventory/src/Market.Inventory.IoC/Configuration/ConfigServices.cs
@@ -1,5 +1,6 @@
 using Market.Inventory.Application.Interfaces;
 using Market.Inventory.Application.Services;
+using Market.Inventory.CrossCutting.Logger;
 using Microsoft.Extensions.DependencyInjection;
 
 
@@ -10,7 +11,7 @@ namespace Market.Inventory.IoC.Configuration
         public static IServiceCollection ConfigAppServices(this IServiceCollection services)
         {
             services.AddScoped<IProductService, ProductService>();
-
+            services.AddSingleton(typeof (ILoggerService<>), typeof (LoggerService<>));
             return services;
         }
     }
